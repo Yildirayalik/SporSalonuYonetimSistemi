6c064121fcdd04225bf66a594cbb51fc2098862f
Yildirayalik <168758304+Yildirayalik@users.noreply.github.com>
2025-12-15 22:04:26 +0300

    Update Member.cs

--- RandevuYonetimSistemi/Models/Member.cs
+++ RandevuYonetimSistemi/Models/Member.cs
@@ -3,6 +3,7 @@
     public class Member
     {
         public int Id { get; set; }
+        //null! ifadeleri ile mevcut yerim boş bırakıpmayacağı anlatılmıştır.
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string Email { get; set; } = null!;
