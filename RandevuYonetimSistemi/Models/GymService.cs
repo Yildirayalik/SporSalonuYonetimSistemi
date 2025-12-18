edcee6184c2676a497c2913ef23bc072f8123a2c
Yildirayalik <168758304+Yildirayalik@users.noreply.github.com>
2025-12-18 22:51:15 +0300

    Update GymService.cs

--- RandevuYonetimSistemi/Models/GymService.cs
+++ RandevuYonetimSistemi/Models/GymService.cs
@@ -3,6 +3,7 @@
     public class GymService
     {
         //ID UU9ID olur
+        // null! o değerin null olamayacağını anlatır
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
